917a2fce75705fb79b1a416f63cfe2756aee0731
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-06-23 00:03:56 +0100

    Get stable deployment of infra and rbac

--- rbac/users.tf
+++ rbac/users.tf
@@ -2,7 +2,8 @@ resource "snowflake_user" "user_ANALYST" {
   provider             = snowflake.security
   name                 = "ANALYST"
   login_name           = "ANALYST"
-  default_role         = "IWDEMO"
+  default_role         = "PUBLIC"
+  password             = "replace"
   must_change_password = "true"
   comment              = "ADEWBERRY Testing"
 
