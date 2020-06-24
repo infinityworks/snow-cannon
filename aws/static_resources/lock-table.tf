1715bf11fb94666bf7fa5e2672099b7a6a9fe9c6
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-06-25 00:19:51 +0100

    Add templated resources to create remote state bucket and lock table.

---
+++ aws/static_resources/lock-table.tf
@@ -0,0 +1,9 @@
+resource "aws_dynamodb_table" "lock-table" {
+  name         = "${var.project}-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
