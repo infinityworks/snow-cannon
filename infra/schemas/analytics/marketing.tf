3f42888b97f3e0c72d690ef0c56ce34c1622d742
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-06-27 18:34:03 +0100

    Move snowflake resources into Snowflake dir

--- infra/schemas/analytics/marketing.tf
+++
@@ -1,9 +0,0 @@
-module "databases" {
-  source = "../../databases"
-}
-
-resource "snowflake_schema" "schema_Marketing" {
-  name     = "Marketing"
-  database = module.databases.analytics_database
-  comment  = "ADEWERRY testing"
-}
