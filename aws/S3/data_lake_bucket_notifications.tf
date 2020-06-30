2db89f5cc9aee73a92f470dba85150b06ed78168
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-06-30 22:29:25 +0100

    Update the README to include pipe deployment. Rename static_resources to state_resources. Update .gitignore for all unwanted terraform filetypes. Remove Dockerfile as it is not needed for now.

--- aws/S3/data_lake_bucket_notifications.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.data_lake.id
-
-  queue {
-    queue_arn = var.snowpipe_queue_arn
-    events    = ["s3:ObjectCreated:*"]
-  }
-}
