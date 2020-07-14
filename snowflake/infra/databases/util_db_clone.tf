8b08dae30dab8d10ae104b9162e1ed6903484a8e
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-07-14 22:12:26 +0100

    Alter the way terraform references the role names, from environments.tfvars to the roles remote state file

--- snowflake/infra/databases/util_db_clone.tf
+++ snowflake/infra/databases/util_db_clone.tf
@@ -1,7 +1,7 @@
 resource "snowflake_database" "db_UTIL_CLONE" {
   name                        = "UTIL_CLONE"
   from_database               = "UTIL_DB"
-  comment                     = "ADEWERRY testing"
+  comment                     = "testing"
   data_retention_time_in_days = 14
 }
 
@@ -10,10 +10,9 @@ resource "snowflake_database_grant" "usage_grants_on_db_UTIL_CLONE" {
   privilege     = "USAGE"
 
   roles = [
-    var.role_BI_ANALYST,
-    var.role_DATA_ANALYST,
+    data.terraform_remote_state.role_info.outputs.BI_ANALYST_name,
+    data.terraform_remote_state.role_info.outputs.DATA_ANALYST_name,
   ]
-
 }
 
 resource "snowflake_database_grant" "modify_grants_on_db_UTIL_CLONE" {
@@ -21,8 +20,7 @@ resource "snowflake_database_grant" "modify_grants_on_db_UTIL_CLONE" {
   privilege     = "MODIFY"
 
   roles = [
-    var.role_BI_ANALYST,
-    var.role_DATA_ANALYST,
+    data.terraform_remote_state.role_info.outputs.BI_ANALYST_name,
+    data.terraform_remote_state.role_info.outputs.DATA_ANALYST_name,
   ]
-
 }
--- snowflake/infra/databases/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_BI_ANALYST" {
-  type        = string
-  description = "Name for the role: BI_ANALYST"
-}
-
-variable "role_DATA_ANALYST" {
-  type        = string
-  description = "Name for the role: DATA_ANALYST"
-}
