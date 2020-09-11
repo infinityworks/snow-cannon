757dde5a7506e3114ec90eea2dc500ba3597856d
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-09-12 00:38:22 +0100

    - Lint README - Revert terraform version to a compatible release. - Export env var TF_PLUGIN_CACHE_DIR

--- aws/state_resources/dynamoDB/main.tf
+++ aws/state_resources/dynamoDB/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.12"
   backend "s3" {
   }
 }
 
 provider "aws" {
-  region  = var.region
+  region  = var.aws_region
   version = "~> 3.5.0"
 }
