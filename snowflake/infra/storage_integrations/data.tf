4e3c27e3d3f205a667f5fb841247a885f0174e11
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-09-12 00:24:27 +0100

    - Bug in Terraform 13 / snowflake provider resource storage integrations; revert to terraform 12. - Create storage_integrations module - Rename module resources to improve module chaining readability.

--- snowflake/infra/storage_integrations/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "data_lake" {
-  backend = "s3"
-  config = {
-    bucket = "snow-cannon-remote-state"
-    key    = "persistence/s3/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
