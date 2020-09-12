c18aa60e9e71feb60cdfe42c43af7360f41fc218
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-09-12 01:16:53 +0100

    - Create stages module - Remove previous examples and replace with module example

--- snowflake/infra/stages/main.tf
+++ snowflake/infra/stages/main.tf
@@ -1,19 +1,25 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.12.29"
 
   backend "s3" {
-  }
-
-  required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.15.0"
-    }
   }
 }
 
 provider "snowflake" {
-  account = "yr26953"
-  region  = "eu-west-1"
-  role    = "ACCOUNTADMIN"
+  account = var.snowflake_account
+  region  = var.snowflake_region
+  role    = "SYSADMIN"
+}
+
+provider "aws" {
+  region  = var.aws_region
+  version = "~> 3.5.0"
+}
+
+module "stage_example" {
+  source         = "../modules/stages-module/"
+  s3_bucket_name = "snow-cannon-data-lake"
+  s3_path        = "key1/key2"
+  database       = "ANALYTICS"
+  schema         = "PUBLIC"
 }
