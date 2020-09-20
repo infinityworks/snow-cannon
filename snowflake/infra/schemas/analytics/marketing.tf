a6efe11ae0c165778e286c3690bedc58db8a5673
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-09-20 16:55:38 +0100

    Revert to tf12 and update file structure to current standard

--- snowflake/infra/schemas/analytics/marketing.tf
+++ snowflake/infra/schemas/analytics/marketing.tf
@@ -1,5 +1,5 @@
 resource "snowflake_schema" "schema_marketing" {
   name     = "Marketing"
-  database = data.terraform_remote_state.snowpipe_database.outputs.analytics_db_name
-  comment  = "ADEWERRY testing"
+  database = data.terraform_remote_state.databases.outputs.analytics_db_name
+  comment  = "Snow-cannon testing"
 }
