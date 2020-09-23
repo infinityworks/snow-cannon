5897a408cc6ee00739cb4dd64e462db3d1f7d7c3
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-09-23 16:40:47 +0100

    Add required providers to all snowflake resource main.tf files

--- snowflake/infra/schemas/analytics/main.tf
+++ snowflake/infra/schemas/analytics/main.tf
@@ -1,7 +1,11 @@
 terraform {
-  required_version = ">= 0.12.29"
-
-  backend "s3" {
+  required_version = ">= 0.13.2"
+  backend "s3" {}
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.15.0"
+    }
   }
 }
 
