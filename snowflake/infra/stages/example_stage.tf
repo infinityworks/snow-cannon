f3181c0f0193d894f4d06886a6a9e681c7558c2a
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-10-09 23:31:30 +0100

    - Major refactor: change environment configuration to use workspaces. Each envs workspace is contained in a single remote state bucket. Using workspaces allows us to manage the provider config and account information from a single source `terraform-config/providers.tf`. - Upgrade snowflake provider to 0.17.1

--- snowflake/infra/stages/example_stage.tf
+++ snowflake/infra/stages/example_stage.tf
@@ -1,10 +1,6 @@
 module "stage_example" {
   source         = "../modules/stages-module/"
-<<<<<<< HEAD
   s3_bucket_name = "snow-cannon-data-lake-${local.formatted_env}"
-=======
-  s3_bucket_name = "snow-cannon-data-lake-${local.env}"
->>>>>>> Separate proviers and example stage
   s3_path        = "key2"
   database       = "ANALYTICS"
   schema         = "PUBLIC"
