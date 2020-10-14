5150d542b94d2dbfacbedf01dc79916bba94a01b
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-10-14 22:36:18 +0100

    Move Snowflake modules one directory higher. This is to allow extension for rbac modules and all snowflake modules will reside in one logical place. There are two sources of module: `.aws/modules` and `.snowflake/modules`. This also gives more clarity in the `.snowflake/infra` directory that all sub directories contain resources to be deployed.

--- snowflake/infra/pipes/example-pipe/example_pipe.tf
+++ snowflake/infra/pipes/example-pipe/example_pipe.tf
@@ -1,5 +1,5 @@
 module "example_pipe" {
-  source         = "../../modules/snowpipe-module/"
+  source         = "../../../modules/snowpipe-module/"
   s3_bucket_name = "snow-cannon-data-lake-${local.formatted_env}"
   s3_path        = "key3"
   database       = "ANALYTICS"
