75f85117cb798402777890d8f0cc2a9ad0532560
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2020-10-15 12:24:21 +0100

    Add snowpipe example for transaction data (#54)

--- snowflake/infra/pipes/example-pipe/example_pipe.tf
+++ snowflake/infra/pipes/example-pipe/example_pipe.tf
@@ -1,7 +1,7 @@
 module "example_pipe" {
   source         = "../../../modules/snowpipe-module/"
   s3_bucket_name = "snow-cannon-data-lake-${local.formatted_env}"
-  s3_path        = "key3"
+  s3_path        = "transactions"
   database       = "ANALYTICS"
   schema         = "PUBLIC"
 }
