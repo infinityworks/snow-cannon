dab148173a08b9a2a9f782363132b40b81237659
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2020-12-01 19:45:33 +0000

    Alter setup script to avoid changing test files (#61)

--- project_setup.py
+++ project_setup.py
@@ -3,6 +3,10 @@ import re
 
 
 def replace_project_name(current_project_name="snow-cannon", dirs_to_glob="./**/*.tf"):
+    globbed_files = set(glob.iglob(dirs_to_glob, recursive=True))
+    ignore_files = set(glob.iglob("./tests/**/*.tf", recursive=True))
+    files_to_modify = globbed_files - ignore_files
+
     project_name = input(
         "Enter your project name; this is used in naming resources like the AWS S3 buckets for remote state and data lake:\n"
     )
@@ -10,7 +14,7 @@ def replace_project_name(current_project_name="snow-cannon", dirs_to_glob="./**/
 
     print(f"Project name: {project_name_replace_specials}")
 
-    for filepath in glob.iglob(dirs_to_glob, recursive=True):
+    for filepath in files_to_modify:
         with open(filepath) as file:
             file_contents = file.read()
         file_contents = file_contents.replace(
