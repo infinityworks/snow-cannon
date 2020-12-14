efdeb8c6adef587ef8a68065677fec51644c8229
Adam Dewberry <adam.dewberry@infinityworks.com>
2020-12-14 22:48:05 +0000

    Create CI user with inline policies to execute Terraform

---
+++ aws/iam-ci-user/variables.tf
@@ -0,0 +1,16 @@
+variable "workspace_name" {
+  # (https://github.com/hashicorp/terraform/issues/22802)"
+  default     = null
+  description = "The actual name of the cloud workspace must be passed in when running inside terraform cloud. This variable should only be set within a workspace defined within terraform cloud."
+  type        = string
+}
+
+variable "path" {
+  description = "IAM role path"
+  default     = null
+}
+
+variable "owner" {
+  description = "The owner of this resource"
+  default     = ""
+}
