ba3476a0bb0090664f649a717b7b2c8761731a06
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2022-11-18 20:53:43 +0000

    Refactor integration deployments to use AWS OpenID to authenticate with GitHub Actions

--- snowflake/infra/storage_integrations/example_integration.tf
+++ snowflake/infra/storage_integrations/example_integration.tf
@@ -4,9 +4,9 @@ module "create_storage_integration_with_iam" {
   s3_path        = "example-integration"
 }
 
-module "create_storage_integration" {
-  source                            = "../../modules/storage-integrations-module/storage-integrations-base/"
-  s3_bucket_name                    = "snow-cannon-data-lake-${local.config.env_formatted.lower}"
-  s3_path                           = "example-integration-2"
-  storage_integration_IAM_role_name = "to-ingest-snowflake-storage-integration-role"
-}
+# module "create_storage_integration" {
+#   source                            = "../../modules/storage-integrations-module/storage-integrations-base/"
+#   s3_bucket_name                    = "snow-cannon-data-lake-${local.config.env_formatted.lower}"
+#   s3_path                           = "example-integration-2"
+#   storage_integration_IAM_role_name = "to-ingest-snowflake-storage-integration-role"
+# }
