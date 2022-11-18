c23b142afecfdcffa50b21b215cfa83b6ad931c5
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2022-11-18 20:24:42 +0000

    Refactor database identifiers to lower case and use AWS OpenID to authenticate with GitHub Actions

--- snowflake/infra/databases/analytics_db.tf
+++ snowflake/infra/databases/analytics_db.tf
@@ -1,25 +1,25 @@
-resource "snowflake_database" "db_ANALYTICS" {
+resource "snowflake_database" "analytics" {
   name                        = "ANALYTICS"
   comment                     = "Data presented in the analytics layer"
   data_retention_time_in_days = 14
 }
 
-resource "snowflake_database_grant" "usage_grants_on_db_ANALYTICS" {
-  database_name = snowflake_database.db_ANALYTICS.name
+resource "snowflake_database_grant" "usage_grants_on_db_analytics" {
+  database_name = snowflake_database.analytics.name
   privilege     = "USAGE"
 
   roles = [
-    data.terraform_remote_state.role_info.outputs.BI_ANALYST_name,
-    data.terraform_remote_state.role_info.outputs.DATA_ANALYST_name,
+    data.terraform_remote_state.role_info.outputs.bi_analyst_name,
+    data.terraform_remote_state.role_info.outputs.data_analyst_name,
   ]
 }
 
-resource "snowflake_database_grant" "modify_grants_on_db_ANALYTICS" {
-  database_name = snowflake_database.db_ANALYTICS.name
+resource "snowflake_database_grant" "modify_grants_on_db_analytics" {
+  database_name = snowflake_database.analytics.name
   privilege     = "MODIFY"
 
   roles = [
-    data.terraform_remote_state.role_info.outputs.BI_ANALYST_name,
-    data.terraform_remote_state.role_info.outputs.DATA_ANALYST_name,
+    data.terraform_remote_state.role_info.outputs.bi_analyst_name,
+    data.terraform_remote_state.role_info.outputs.data_analyst_name,
   ]
 }
