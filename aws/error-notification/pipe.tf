8b40294c6083322259f90c7a36e6ad96d3aa6a98
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2022-12-19 18:17:41 +0000

    build error notification channel (#95)
    * Create SQS subscription for Snowpipe error notification SNS channel/topic. Trigger lambda to digest the snowpipe error message and generate cloudwatch logs and metric based upon the SNS topic arn and error type to understand why a file failed to be delivered from S3 to Snowflake - via a pipe
    
    * Create SQS subscription for Snowpipe error notification SNS channel/topic. Trigger lambda to digest the snowpipe error message and generate cloudwatch logs and metric based upon the SNS topic arn and error type to understand why a file failed to be delivered from S3 to Snowflake - via a pipe
    
    * Abstract Snowpipe specific error mapping to class to allow the cloudwatch logging and metric class to be generalised
    
    * Create test for SnowPipeObservabilityFormatting.create_error_map_from_event
    
    * Remove Terraform docs linting checks as they prohibit READMEs in sub directories. Add README for pipe error-notifications of how the monitoring works and how to run an integration test by forcing a pipe error
    
    * Move all Snowflake pipe logic from aws/error-notification/lambda/cloudwatch_client.py into aws/error-notification/lambda/snowpipe_event_formatting.py to cleaning separate the abstractions. Add test for SnowPipeEventHandler.convert_timestamp_to_epoch_time_in_milliseconds

---
+++ aws/error-notification/pipe.tf
@@ -0,0 +1,12 @@
+module "test_pipe_to_fail" {
+  source            = "../../snowflake/modules/snowpipe-module/"
+  s3_bucket_name    = "snow-cannon-data-lake-${local.config.env_formatted.lower}"
+  s3_path           = "customers"
+  database          = "ANALYTICS"
+  schema            = "PUBLIC"
+  error_integration = "SNOWPIPE_ERROR_CHANNEL"
+  file_format       = "CSV"
+  field_delimiter   = "\t"
+  record_delimiter  = "\n"
+  filter_suffix     = ".csv"
+}
